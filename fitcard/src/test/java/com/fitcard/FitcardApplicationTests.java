c4fce14c67ea8590a4b264d99931097241db354f
LimGayoung <ygy920@gmail.com>
2024-09-04 14:31:22 +0900

    init: project

---
+++ fitcard/src/test/java/com/fitcard/FitcardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitcardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
