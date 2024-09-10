0cf96ab291c9894f58e4dc578cbef675b80e660c
LimGayoung <ygy920@gmail.com>
2024-09-10 17:29:23 +0900

    #12 feat: Kakao Local API로 카테고리별 장소 검색 구현

---
+++ Backend/fitcard/src/main/java/com/fitcard/infra/kakao/model/LocalInfo.java
@@ -0,0 +1,37 @@
+package com.fitcard.infra.kakao.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@ToString
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LocalInfo {
+
+    private String categoryGroupCode;
+    private String categoryGroupName;
+    private String categoryName;
+    private String id;
+    private String phone;
+    private String placeName;
+    private String placeUrl;
+    private String roadAddressName;
+    private String x;
+    private String y;
+
+    public static LocalInfo from(KakaoCategoryLocalApiResponses.KakaoCategoryLocalApiResponse response) {
+        return new LocalInfo(response.getCategory_group_code(),
+                response.getCategory_group_name(),
+                response.getCategory_name(),
+                response.getId(),
+                response.getPhone(),
+                response.getPlace_name(),
+                response.getPlace_url(),
+                response.getRoad_address_name(),
+                response.getX(),
+                response.getY());
+    }
+}
---
+++ Backend/fitcard/src/main/java/com/fitcard/infra/kakao/service/KakaoLocalService.java
@@ -0,0 +1,66 @@
+package com.fitcard.infra.kakao.service;
+
+import com.fitcard.infra.kakao.model.KakaoCategoryLocalApiResponses;
+import com.fitcard.infra.kakao.model.KakaoCategoryRequestQueryParameter;
+import com.fitcard.infra.kakao.model.LocalInfo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
+
+@Slf4j
+@Service
+public class KakaoLocalService {
+
+    private final WebClient webClient;
+    private final String CATEGORY_LOCAL_REQUEST_URI;
+    private final String ORIGIN_URI;
+    private final String KAKAO_API_KEY;
+
+
+    public KakaoLocalService(WebClient webClient,
+                             @Value("${webclient.kakao.origin}") String originUri,
+                             @Value("${webclient.kakao.local.category}") String categoryUri,
+                             @Value("${webclient.kakao.apikey}") String kakoApiKey) {
+        this.webClient = webClient;
+        this.ORIGIN_URI = originUri;
+        this.CATEGORY_LOCAL_REQUEST_URI = categoryUri;
+        this.KAKAO_API_KEY = kakoApiKey;
+    }
+
+
+    public List<LocalInfo> getLocalWithCategoryFromKakao(KakaoCategoryRequestQueryParameter parameter){
+
+        //1. request를 바탕으로 요청 url 만들기
+//        String x = "127.05897078335246";
+//        String y = "37.506051888130386";
+        String radius = "20000";
+        String requestUrl = ORIGIN_URI+CATEGORY_LOCAL_REQUEST_URI+"?category_group_code="+parameter.getCategory_group_code()
+                +"&x="+parameter.getX()+"&y="+parameter.getY()
+                +"&radius="+parameter.getRadius()+"&page="+parameter.getPage();
+
+        log.info("requestUrl: {}", requestUrl);
+
+        //2. webClient.get으로 요청해서 LocalInfo list 받기
+
+        KakaoCategoryLocalApiResponses responses = webClient.get()
+                .uri(requestUrl)
+                .header("Authorization", "KakaoAK " + KAKAO_API_KEY)
+                .retrieve()
+                .bodyToMono(KakaoCategoryLocalApiResponses.class)
+                .doOnError(throwable -> {
+                    log.info("kakao api error: {} localizedMessage: {}", throwable.getMessage(), throwable.getLocalizedMessage());
+                })
+                .block();
+
+        if (responses != null) {
+            return responses.getDocuments().stream()
+                    .map(LocalInfo::from)
+                    .toList();
+        }
+
+        return List.of();
+    }
+}
