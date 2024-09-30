3a25aa0aa00ac970ae4bcd215c9827067c7e6f79
LimGayoung <ygy920@gmail.com>
2024-09-30 10:28:25 +0900

    #45 feat: 사용자 카드 이용내역 조회 API 구현

--- Backend/financial/src/main/java/com/financial/domain/bank/usercardpayment/controller/BankUserCardPaymentController.java
+++ Backend/financial/src/main/java/com/financial/domain/bank/usercardpayment/controller/BankUserCardPaymentController.java
@@ -1,4 +1,25 @@
 package com.financial.domain.bank.usercardpayment.controller;
 
+import com.financial.domain.bank.usercardpayment.model.dto.request.BankUserCardPaymentGetRequest;
+import com.financial.domain.bank.usercardpayment.model.dto.response.BankUserCardPaymentGetResponses;
+import com.financial.domain.bank.usercardpayment.service.BankUserCardPaymentService;
+import com.financial.global.response.Response;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/banks/usercard/payments")
 public class BankUserCardPaymentController {
+
+    private final BankUserCardPaymentService bankUserCardPaymentService;
+
+    @GetMapping("/")
+    public Response<BankUserCardPaymentGetResponses> getUserCardPayments(@RequestParam Long bankUserCardId,
+                                                                             @RequestParam Integer lastId) {
+        BankUserCardPaymentGetResponses response = bankUserCardPaymentService.getBankUserCardPayments(new BankUserCardPaymentGetRequest(bankUserCardId, lastId));
+        return Response.SUCCESS(response);
+    }
 }
