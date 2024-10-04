8619c3a634e731e6b9cb592b497f9fb677b9ced1
LimGayoung <ygy920@gmail.com>
2024-10-04 10:56:14 +0900

    fix: MemberCardPerformnace에 cardPerformance 정보 추가

--- Backend/fitcard/src/main/java/com/fitcard/domain/membercard/performance/model/MemberCardPerformance.java
+++ Backend/fitcard/src/main/java/com/fitcard/domain/membercard/performance/model/MemberCardPerformance.java
@@ -31,24 +31,37 @@ public class MemberCardPerformance extends BaseEntity {
     @NotNull
     private Integer lastFinancialId;
 
+    @NotNull
+    private Integer cardPerformanceId;
+
+    @NotNull
+    private Integer cardPerformanceLevel;
+
     @ManyToOne
     @JoinColumn(name = "member_card_id", nullable = false)
     private MemberCardInfo memberCard;
 
-    private MemberCardPerformance(Integer amount, Integer year, Integer month, MemberCardInfo memberCard) {
+    private MemberCardPerformance(Integer amount, Integer year, Integer month, MemberCardInfo memberCard, Integer cardPerformanceId, Integer cardPerformanceLevel) {
         this.amount = amount;
         this.year = year;
         this.month = month;
         this.memberCard = memberCard;
         this.lastFinancialId = 0;
+        this.cardPerformanceId = cardPerformanceId;
+        this.cardPerformanceLevel = cardPerformanceLevel;
     }
 
     public static MemberCardPerformance empty(Integer year, Integer month, MemberCardInfo memberCardInfo) {
-        return new MemberCardPerformance(0, year, month, memberCardInfo);
+        return new MemberCardPerformance(0, year, month, memberCardInfo, 0, 1);
     }
 
     public void setLastFinancialId(Integer lastFinancialId, Integer amount) {
         this.lastFinancialId = lastFinancialId;
         this.amount = amount;
+    }
+
+    public void setCardPerformance(Integer cardPerformanceId, Integer cardPerformanceLevel) {
+        this.cardPerformanceId = cardPerformanceId;
+        this.cardPerformanceLevel = cardPerformanceLevel;
     }
 }
