9ff4ef7e517c359cf41582cb78615863cf665225
qkrtjdwo5662 <qkrtjdwo5662@naver.com>
2024-10-06 13:09:22 +0900

    #30 feat: 가맹점 사용자 카드 혜택 계산

--- Backend/fitcard/src/main/java/com/fitcard/domain/merchant/branch/repository/BranchRepository.java
+++ Backend/fitcard/src/main/java/com/fitcard/domain/merchant/branch/repository/BranchRepository.java
@@ -96,4 +96,29 @@ public interface BranchRepository extends JpaRepository<Branch, Long> {
             "GROUP BY mc.card_version_id, ci.name, cv.image_url, m.name ", nativeQuery = true)
     List<Object[]> findBranchMemberCardBenefit(@Param("loginId") Integer loginId, @Param("merchantBranchId") Integer merchantBranchId);
 
+    @Query(value = "SELECT " +
+            "max(CASE cb.benefit_type " +
+            "WHEN 'PERCENT_DISCOUNT' THEN CONCAT(cb.benefit_value, '% 할인') " +
+            "WHEN 'PERCENT_REWARD' THEN CONCAT(cb.benefit_value, '% 적립') " +
+            "WHEN 'PERCENT_CASHBACK' THEN CONCAT(cb.benefit_value, '% 캐쉬백') " +
+            "WHEN 'MILEAGE_REWARD' THEN CONCAT(cb.benefit_value, ' 마일리지 적립') " +
+            "WHEN 'AMOUNT_DISCOUNT' THEN CONCAT(cb.benefit_value, '원 할인') " +
+            "WHEN 'POINT_REWARD' THEN CONCAT(cb.benefit_value, ' 포인트 적립') " +
+            "WHEN 'AMOUNT_CASHBACK' THEN CONCAT(cb.benefit_value, '원 캐쉬백') " +
+            "WHEN 'LITER_DISCOUNT' THEN CONCAT('리터당 ', cb.benefit_value, ' 할인') " +
+            "ELSE '기타 혜택' " +
+            "END) AS benefit_description, round(MAX(:money*(100 - cb.benefit_value)/100), 0) AS benefit " +
+            "FROM merchant_branch mb " +
+            "left join merchant_card mc on mb.merchant_id = mc.merchant_id " +
+            "left join merchant m on mb.merchant_id = m.merchant_id " +
+            "left join member_card memc on mc.card_version_id = memc.card_version_id " +
+            "left join card_version cv on mc.card_version_id = cv.id " +
+            "left join card_info ci on cv.card_info_id = ci.id " +
+            "left join member_card_performance mcp on memc.member_card_id = mcp.member_card_id " +
+            "left join card_benefit cb on mcp.card_performance_id = cb.card_performance_id " +
+            "left join card_performance cp on mcp.card_performance_level = cp.level " +
+            "WHERE mb.merchant_branch_id = :merchantBranchId AND memc.member_id = :loginId AND mcp.member_card_performance_id is not null " +
+            "and mcp.month = month(now()) and mc.card_version_id = :cardVersionId " +
+            "GROUP BY mc.card_version_id, ci.name, cv.image_url, m.name ", nativeQuery = true)
+    List<Object[]> calculateBenefit(@Param("loginId") Integer loginId, @Param("merchantBranchId") Integer merchantBranchId, @Param("cardVersionId") Integer cardVersionId, @Param("money") int money);
 }
